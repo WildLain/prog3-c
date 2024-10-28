0aabdf9ff5a1084c1efb79de6b7b89d7cb974462
Tran Nha-Dan Tran <trannhadan.tran@student.hs-rm.de>
2024-10-28 17:49:39 +0100

    aufgabe 2 uebungsblatt 2

---
+++ b02a01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+long double x, w, new_w, abstand;
+int main(void)
+{
+    scanf("%Lf", &x);
+    w = (x + 1) / 2;
+    do
+    {
+        new_w = (w + (x / w)) / 2;
+        abstand = w - new_w;
+        w = new_w;
+    } while (abstand > 1e-7L);
+
+    printf("%Lg", w);
+    return 0;
+}
+
