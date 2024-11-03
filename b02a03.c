9c206b4309fd9bd127fd451beace29aad3a6c2e6
Tran Nha-Dan Tran <trannhadan.tran@student.hs-rm.de>
2024-11-03 17:21:26 +0100

    selectionsort impl

--- b02a03.c
+++ b02a03.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 enum {VEKLAENGE=20};
-int  i, j, n, min, max, v[VEKLAENGE], v2[VEKLAENGE];
+int  i, j, n, min, tmp, v[VEKLAENGE], v2[VEKLAENGE];
 
 void printMyVec(void) {
-    for(j = 0; j < i; j++) {
+    for(i = 0; i < n; i++) {
         printf("%6d\n", v[j]);
     }
 }
 
 void selectionSort(int v[VEKLAENGE]) {
-    for(n = 0; n < i-2; n++) {
-        
+    for(i = 0; i < n-1; i++) {
+        min = i;
+        for(j = i+1; j < n; j++) {
+            if(v[min] > v[j]) {
+                min = j;
+                tmp = v[i];
+                v[i] = v[min];
+                v[min] = tmp;
+            }
+        }
     }
 }
 
 int main (void) {
     int input;
-    i = 0;
+    n = 0;
     while(scanf("%d", &input) != EOF) {
-        v[i] = input;
-        i++;
+        v[n] = input;
+        n++;
     }
+    printMyVec();
+    selectionSort(v[VEKLAENGE]);
     printMyVec();
 }
