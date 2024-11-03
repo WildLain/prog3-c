a1cbcdec425c3cca3db67a34cf1fa8b85b188102
Tran Nha-Dan Tran <trannhadan.tran@student.hs-rm.de>
2024-11-03 14:18:27 +0100

    aufgabe 2 von blatt 2 beendet

--- b0202.c
+++ b0202.c
@@ -1,27 +1,48 @@
 #include <stdio.h>
+#include <string.h>
 
-int i = 0, input;
-int binaryVector[16];
-unsigned int intAsBinary;
+int i, shift, len;
+unsigned int input, output, a, b, rAnd, rOr, rXor;
+char bStr[16];
 
 void schreibBit(unsigned z)
 {
-    do
-    {
-        binaryVector[i] = z % 2;
-        z /= 2;
-        i++;
-    } while (z > 0);
-
-    for (i -= 1; i >= 0; i--)
-    {
-        printf("%d", binaryVector[i]);
+    for (i=15 ; i >= 0 ; i--) {
+        printf("%d", (z & 1<<i) != 0);
     }
+    printf("\n");
+}
+
+unsigned int liesBit(void) {
+    output = 0;
+    scanf("%s", bStr);
+    len = strlen(bStr);
+    for(i = 0; i < len; i++) {
+        output *= 2;
+        if(bStr[i] == '1') {
+            output++;
+        }
+    }
+    if(len < 16) {
+        shift = 15 - len +1;
+        output << shift;
+    }
+    /*printf("%d\n", output);*/
+    return output;
 }
 
 int main(void)
 {
-    scanf("%u", &input);
-    schreibBit(input);
+
+    a = liesBit();
+    b = liesBit();
+
+    printf("a & b = ");
+    schreibBit(a & b);
+    printf("a | b = ");
+    schreibBit(a | b);
+    printf("a ^ b = ");
+    schreibBit(a ^ b);
+
     return 0;
 }
