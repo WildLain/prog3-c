14434219954fd6bb2d6e4259ed02d01b43fd3ef6
Tran Nha-Dan Tran <trannhadan.tran@student.hs-rm.de>
2024-11-03 18:11:33 +0100

    Aufgabe 3 zu ende

--- b02a03.c
+++ b02a03.c
@@ -1,35 +1,46 @@
 #include <stdio.h>
-enum {VEKLAENGE=20};
-int  i, j, n, min, tmp, v[VEKLAENGE], v2[VEKLAENGE];
+#include <string.h>
 
-void printMyVec(void) {
+enum {VEKLAENGE=20};
+
+void printMyVec(int v[VEKLAENGE], int n) {
+    int i;
     for(i = 0; i < n; i++) {
-        printf("%6d\n", v[j]);
+        printf("%6d\n", v[i]);
     }
 }
 
-void selectionSort(int v[VEKLAENGE]) {
+void selectionSort(int v[VEKLAENGE], int n) {
+    int i, j, min, tmp;
     for(i = 0; i < n-1; i++) {
         min = i;
         for(j = i+1; j < n; j++) {
             if(v[min] > v[j]) {
                 min = j;
-                tmp = v[i];
-                v[i] = v[min];
-                v[min] = tmp;
             }
+        }
+        if (min != i) {
+            tmp = v[i];
+            v[i] = v[min];
+            v[min] = tmp;
         }
     }
 }
 
 int main (void) {
-    int input;
-    n = 0;
-    while(scanf("%d", &input) != EOF) {
+    char line[VEKLAENGE];
+    int v[VEKLAENGE];
+    int input, n = 0;
+    while(fgets(line, VEKLAENGE, stdin) && n < VEKLAENGE) {
+        if(sscanf(line, "%d", &input) == 1) {
+            v[n] = input;
+            n++;
+        }
+    }
+   /*while(scanf("%d", &input) != EOF) {
         v[n] = input;
         n++;
-    }
-    printMyVec();
-    selectionSort(v[VEKLAENGE]);
-    printMyVec();
+    }*/
+    selectionSort(v, n);
+    printMyVec(v, n);
 }
