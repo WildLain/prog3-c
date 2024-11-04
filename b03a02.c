e800dc6026476463712dc8ee4320c7c9e1e66998
Nha-Dan <trannhadan.tran@student.hs-rm.de>
2024-11-04 19:05:25 +0100

    Blatt 3 Aufgabe 2 In Progress

---
+++ b03a02.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+enum {LINE_LENGTH=100};
+const char alph[] = "abcdefghijklmnopqrstuvwxyz";
+const char cryp[] = "wgsnqcdvmeyluzoabhrjfkxipt";
+
+char encryptF(void) {
+    char line[LINE_LENGTH], input;
+    char *p, c, e;
+    int i;
+
+    for(i = 0; i < 26; i++) {
+        if(c == alph[i]) {
+            e = cryp[i];
+            break;
+        }
+    }
+
+    return e;
+    /*while(scanf("%c", &input) != EOF) {
+        if(scanf("%c", &input) == 1) {
+
+        }        
+    } */
+}
+
+void decryptF(void) {
+    char line[LINE_LENGTH];
+    
+}
+
+int main(int argc, char *argv[]) {
+    char line[LINE_LENGTH];
+    char input, encrypt[] = "encrypt", decrypt[] = "decrypt";
+
+    if(argc != 2) {
+        printf("Verwendung: %s encrypt/decrypt", argv[0]);
+        return 1;
+    }
+
+    if(strcmp(argv[1], encrypt) == 0) {
+        encryptF();
+    }
+    else if(strcmp(argv[1], decrypt) == 0) {
+        decryptF();
+    }
+
+    return 0;
+}
