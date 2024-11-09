e3e9b31e1aeac07ab908a92b1a0fdd032dff5a6e
Tran Nha-Dan Tran <trannhadan.tran@student.hs-rm.de>
2024-11-09 18:38:00 +0100

    B03a03 fertig I guess?

---
+++ b03a03.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+enum { slen = 100, rlen = 7};
+
+
+void soundex(const char s[], char res[]) {
+    /*char c = c - 'a' + 'A';*/
+    int i, z, j = 1;
+    char c;
+    res[0] = toupper(s[0]);
+    for(i = 1; i < strlen(s); i++) {
+        z = 0;
+        c = toupper(s[i]);
+
+        switch(c) 
+        {
+            case 'B':
+            case 'F':
+            case 'P':
+            case 'V':
+                z = 1;
+                break;
+            case 'C':
+            case 'G':
+            case 'J':
+            case 'K':
+            case 'Q':
+            case 'S':
+            case 'X':
+            case 'Z':
+                z = 2;
+                break;
+            case 'D':
+            case 'T':
+                z = 3;
+                break;
+            case 'L':
+                z = 4;
+                break;
+            case 'M':
+            case 'N':
+                z = 5;
+                break;
+            case 'R':
+                z = 6;
+                break;
+            default:
+                break;
+        }
+        if (z != 0) 
+        {
+            if(j < 5) {
+            res[j] = z + '0';
+            j++;
+            }
+            else
+            {
+                res[j] = z + '0';
+            }
+        }
+    }
+    res[6] = '\0';
+    printf("%s\n", res);
+}
+
+int main(void) {
+    char res[rlen] = {'0', '0', '0', '0', '0', '0'};
+    char s[slen];
+
+    while(scanf("%s", s) != EOF)  
+    {
+        soundex(s, res);
+    }
+
+    return 0;
+}
