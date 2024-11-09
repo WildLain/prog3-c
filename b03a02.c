761c2a80d8c453c1f21877e2e3a7b451ba6f36c4
Tran Nha-Dan Tran <trannhadan.tran@student.hs-rm.de>
2024-11-09 16:40:32 +0100

    Blatt 3 Aufgabe 2

--- b03a02.c
+++ b03a02.c
@@ -1,44 +1,62 @@
 #include <stdio.h>
 #include <string.h>
 
-enum {LINE_LENGTH=100};
-const char alph[] = "abcdefghijklmnopqrstuvwxyz";
-const char cryp[] = "wgsnqcdvmeyluzoabhrjfkxipt";
+enum
+{
+    LINE_LENGTH = 100
+};
+char alph[] = "abcdefghijklmnopqrstuvwxyz";
+char cryp[] = "wgsnqcdvmeyluzoabhrjfkxipt";
+char crypt(char c, char src[], char dst[]);
 
-char encryptF(void) {
-    char line[LINE_LENGTH], input;
-    char *p, c;
+char crypt(char c, char src[], char dst[])
+{
+    char line[LINE_LENGTH];
+    char *p;
     int i;
-    
-    for(i = 0; i < 26; i++) {
-        if(c == alph[i]) {
-            p = cryp + i;
-            break;
+
+    if (c >= 'a' && c <= 'z')
+    {
+        for (i = 0; i < strlen(alph); i++)
+        {
+            if (c == src[i])
+            {
+                p = dst + i;
+                break;
+            }
         }
+        return *p;
     }
-
-    return *p;
+    else
+    {
+        return c;
+    }
 }
 
-void decryptF(void) {
-    char line[LINE_LENGTH];
-    
-}
+int main(int argc, char *argv[])
+{
+    char encrypt[] = "encrypt", decrypt[] = "decrypt";
+    char input;
 
-int main(int argc, char *argv[]) {
-    char line[LINE_LENGTH];
-    char input, encrypt[] = "encrypt", decrypt[] = "decrypt";
-
-    if(argc != 2) {
+    if (argc != 2)
+    {
         printf("Verwendung: %s encrypt/decrypt", argv[0]);
         return 1;
     }
 
-    if(strcmp(argv[1], encrypt) == 0) {
-        encryptF();
+    if (strcmp(argv[1], encrypt) == 0)
+    {
+        while ((scanf("%c", &input) != EOF))
+        {
+            printf("%c", crypt(input, alph, cryp));
+        }
     }
-    else if(strcmp(argv[1], decrypt) == 0) {
-        decryptF();
+    else if (strcmp(argv[1], decrypt) == 0)
+    {
+        while ((scanf("%c", &input) != EOF))
+        {
+            printf("%c", crypt(input, cryp, alph));
+        }
     }
 
     return 0;
