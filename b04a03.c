b9980ab247bbbe47f9a9a154dd0bf64e549c8571
qt-1 <trannhadan.tran@student.hs-rm.de>
2024-11-21 18:52:25 +0100

    Final Blatt 4 Aufgabe 3

--- b04a03.c
+++ b04a03.c
@@ -3,20 +3,20 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-char *replace(char *line, char *word)
+void replace(char *line, char *word)
 {
     char *p = line;
     char *p2 = word;
     int i;
-    while(*p != '\n')
+    while(*p != '\0')
     {
         if(*p == *p2)
         {
             for(i = 0; i < strlen(word); i++)
             {
-                p = p + i;
-                p2 = p2 + i;
-                if(*p == *p2)
+                char c1 = p[i];
+                char c2 = p2[i];
+                if(c1 == c2)
                 {
                     continue;
                 }
@@ -25,48 +25,23 @@ char *replace(char *line, char *word)
                     break;
                 }
             }
-            if(i == strlen(word)-1)
+            if(i == strlen(word))
             {
-                p = p - strlen(word)-1;
-                while(isalpha(*p) != 0)
+                for(i = 1; i < strlen(word); i++)
                 {
-                    *p = '*';
-                    p++;
+                    p[i] = '*';
                 }
             }   
         }
         p++;
     }
-
-    /*char *p = strstr(line, word);
-    int i;
-    if (p != NULL)
-    {
-
-        for (i = 1; i < strlen(word); i++)
-        {
-            p[i] = '*';
-        }
-    }*/
-    return line;
 }
 
 int main(int argc, char const *argv[])
 {
-    char input[] = "Liebe Tante, unser Elend unter Glogomir ist unbeschreibbar.";
-    char word[] = "Elend";
-
-    replace(input, word);
-    printf("%s", input);
-
-    /*enum
-    {
-        MAXLEN = 200
-    };
+    enum    { MAXLEN = 200 };
     char input[MAXLEN], word[80];
     int i;
-
-    
     while (fgets(input, MAXLEN, stdin) != NULL)
     {
         size_t len = strlen(input);
@@ -76,11 +51,18 @@ int main(int argc, char const *argv[])
             for (i = 1; i < argc; i++)
             {
                 strcpy(word, argv[i]);
-                strcpy(input, replace(input, word));
+                replace(input, word);
             }
             printf("\n%s\n", input);
             printf("\n");
         }
-    }*/
+    }
+    
+    /*char input[] = "Liebe Tante, unser Elend unter Glogomir ist unbeschreibbar.\n";
+    char word[] = "Elend";
+
+    replace(input, word);
+    printf("%s", input);*/
+
     return 0;
 }
