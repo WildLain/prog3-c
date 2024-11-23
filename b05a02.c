127d1bc04618f683bac65d949ac304d87cfe75e3
qt-1 <trannhadan.tran@student.hs-rm.de>
2024-11-23 13:38:02 +0100

    b05a02 fertig

--- b05a02.c
+++ b05a02.c
@@ -1,52 +1,64 @@
 #include <stdio.h>
 
-double formel(double xVal)
-{
-    return (xVal * xVal) - 9;
+/* Manuelle Berechnung des absoluten Werts */
+double my_fabs(double x) {
+    if (x < 0) {
+        return -x;
+    }
+    return x;
 }
 
-double regula(double x1, double x2, double (*f)(double))
-{
-    double xs;
-    double posXs;
+/* Definition der Funktion regula() zur Bestimmung der Nullstelle einer Funktion f(x) */
+double regula(double x1, double x2, double (*f)(double)) {
+    double xs, fx1, fx2, fx3;
+    
+    /* Sicherstellen, dass f(x1) * f(x2) < 0 ist (Vorzeichenwechsel) */
+    if (f(x1) * f(x2) >= 0) {
+        fprintf(stderr, "Fehler: Vorzeichenwechsel zwischen f(x1) und f(x2) ist nicht gegeben.\n");
+        return -1.0 / 0.0;  /* Gibt -∞ zurück, um anzuzeigen, dass keine Lösung gefunden wurde */
+    }
+    
+    /* Iteration bis die Genauigkeit erreicht ist */
+    do {
+        fx1 = f(x1);
+        fx2 = f(x2);
+        
+        /* Berechnung der neuen Schätzung xs */
+        xs = x1 - fx1 * (x2 - x1) / (fx2 - fx1);
+        fx3 = f(xs);
 
-    if (f(x2) == f(x1))
-    {
-        printf("Fehler: Division durch Null in der Regula-Falsi-Methode.\n");
-        return -1; 
-    }
-
-    xs = x1 - (f(x1) * ((x2 - x1) / (f(x2) - f(x1))));
-    if (xs < 0)
-    {
-        posXs = -xs;
-    }
-    else
-    {
-        posXs = xs;
-    }
-    if (f(posXs) < 1e-6)
-    {
-        return posXs;
-    }
-    if ((f(xs) > 0 && f(x1) > 0) || (f(xs) < 0 && f(x1) < 0))
-    {
-        return regula(xs, x2, f);
-    }
-    else
-    {
-        return regula(x1, xs, f);
-    }
+        /* Wenn das Vorzeichen von f(xs) mit f(x1) übereinstimmt, setze x1 auf xs */
+        if (fx3 * fx1 < 0) {
+            x2 = xs;
+        } else {  /* Andernfalls setze x2 auf xs */
+            x1 = xs;
+        }
+        
+        /* Wiederhole die Berechnung, solange der Fehler nicht klein genug ist */
+    } while (my_fabs(f(xs)) >= 1E-6);  /* Genauigkeit: |f(xs)| < 1E-6 */
+    
+    /* Gibt die Nullstelle zurück */
+    return xs;
 }
 
-int main(void)
-{
-    double (*f)(double) = formel;
-    double result;
-    result = regula(-100, 100, f);
-    if (result != -1) {
-        printf("Gefundene Nullstelle: %f\n", result);  
-    }
+/* Beispielhafte Funktion f(x) = x^2 - 9, deren Nullstellen bekannt sind */
+double f1(double x) {
+    return x * x - 9;  /* f(x) = x^2 - 9 */
+}
 
+int main() {
+    /* Testen der regula() Funktion mit der Funktion f1(x) */
+    double x1 = 0;   /* Startwert 1 */
+    double x2 = 5;   /* Startwert 2 */
+
+    double result = regula(x1, x2, f1);
+    
+    /* NaN-Prüfung: Wenn das Ergebnis -∞ ist, gab es einen Fehler */
+    if (result != result) {  /* Prüft, ob der Wert NaN ist */
+        fprintf(stderr, "Fehler: Keine gültige Nullstelle gefunden.\n");
+    } else {
+        printf("Die Nullstellen: (+/-)%f\n", result);
+    }
+    
     return 0;
 }
---
+++ b05a03.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void replace(char *line, char *word)
+{
+    char *p = line;
+    char *p2 = word;
+    int i;
+    while(*p != '\0')
+    {
+        if(*p == *p2)
+        {
+            for(i = 0; i < strlen(word); i++)
+            {
+                char c1 = p[i];
+                char c2 = p2[i];
+                if(c1 == c2)    continue;
+                else            break;
+            }
+             if(i == strlen(word))
+            {
+                for(i = 1; i < strlen(word); i++)                
+                    {p[i] = '*';}
+            }   
+        }
+        p++;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    enum    { MAXLEN = 200 };
+    char input[MAXLEN], word[80];
+    int i;
+    while (fgets(input, MAXLEN, stdin) != NULL)
+    {
+        int len = strlen(input);
+        if (input[len - 1] == '\n')
+        {
+            input[len - 1] = '\0';
+            for (i = 1; i < argc; i++)
+            {
+                strcpy(word, argv[i]);
+                replace(input, word);
+            }
+            printf("%s\n", input);
+        }
+    }
+    return 0;
+}
