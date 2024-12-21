977d224f21060c078576a2a394d0072d23116fdf
qt-1 <trannhadan.tran@student.hs-rm.de>
2024-12-21 13:10:58 +0100

    b06a02 Final

--- b06a02.c
+++ b06a02.c
@@ -1,53 +1,76 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 #include <stdlib.h>
-
-#define ALPHABET_SIZE  26
-
+#include <ctype.h>
 typedef struct {
     char codezeichen;
     int anzahl;
 } Ele;
 
-/* Vergleichsfunktion für qsort */
-int compare(const void *a, const void *b) {
-    Ele *eleA = (Ele *)a;
-    Ele *eleB = (Ele *)b;
+int compare(const void *e1, const void *e2) {
 
-    /* Sortiere zuerst nach absteigender Hauefigkeit */
-    if(eleB->anzahl != eleA->anzahl) {
-        return eleB->anzahl - eleA->anzahl;
+    /*Wenn anzahl gleich ist, sortiere nach nach Codezeichen*/
+    if(((Ele*)e1)->anzahl != ((Ele*)e2)->anzahl) {
+        return ((Ele*)e2)->anzahl - ((Ele*)e1)->anzahl;
     }
-
-    /* Bei gleicher Häufigkeit nach aufsteigendem Zeichenwert */
-    return eleA->codezeichen - eleB->codezeichen;
+    return ((Ele*) e1)->codezeichen - ((Ele*) e2)->codezeichen;
 }
 
-void analyzeHauefigkeit(FILE *file, Ele *haeufigkeiten) {
-    int ch, i;
+void analyze(FILE *origin) {
+    enum {ALPHALEN = 26};
+    Ele tabelle[ALPHALEN];
+    char ersetzungen[ALPHALEN];
+    int i, ch;
+    char rangfolge[] = "eniastruhdlcmogkwbzfvpjxyq";
 
-    /* initialisiere die Hauefigkeitsstruktur */
-    for (i = 0; i < ALPHABET_SIZE; i++) {
-        haeufigkeiten[i].codezeichen = 'a' + i;
-        haeufigkeiten[i].anzahl = 0;
+    /*Tabelle erstellen*/
+    for(i = 0; i < ALPHALEN; i++) {
+        tabelle[i].codezeichen = 'a' + i;
+        tabelle[i].anzahl = 0;
     }
 
-    /* Zähle die Buchstabenhäufigkeiten */
-    while((ch = fgetc(file)) != EOF) {
+    /*Buchstaben zählen*/
+    while((ch = fgetc(origin)) != EOF) {
         if(isalpha(ch)) {
-            haeufigkeiten[ch - 'a'].anzahl++;
+            tabelle[ch - 'a'].anzahl++;
         }
-    }    
+    }
+    /*tabelle sortieren*/
+    qsort(tabelle, ALPHALEN, sizeof(Ele), compare);
+
+    /*Debug, Hauefigkeitentabelle ausgeben*/
+    for (i = 0; i < ALPHALEN; i++) {
+    printf("Buchstabe: %c, Häufigkeit: %d\n", tabelle[i].codezeichen, tabelle[i].anzahl);
 }
 
-/* Funktion zur Entschlüsselung des Textes */
-void entschluesseln(FILE *file, Ele *haeufigkeiten, const char *klartextReihenfolge) {
-    char ersetzung[ALPHABET_SIZE];
-    int ch, i;
-
-    /* Erstelle die Zuordungstabelle basieren auf der Klartext-Reihenfolge */
-    for (i= 0; i < ALPHABET_SIZE; i++) {
-        ersetzung[haeufigkeiten[i].codezeichen - 'a'] = klartextReihenfolge[i];
+    /*auf Entschlüsselungstabelle abbilden*/
+    for(i = 0; i < ALPHALEN; i++) {
+        /*Stelle im eigentlichen Alphabet: tabelle[i].codezeichen - 'a', 
+        ASCII-zeichen minus dem eigentlichen Zeichen gibt Pos auf dem Index zurück*/
+        ersetzungen[tabelle[i].codezeichen - 'a'] = rangfolge[i];
     }
+
+    rewind(origin);
+    /*Entschlüsseln*/
+    while((ch = fgetc(origin)) != EOF) {
+        if(isalpha(ch)) {
+            printf("%c", ersetzungen[ch-'a']);   
+        }
+        else {
+            printf("%c", ch);
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    FILE *stream;
+    /*argv[1] = "raeuberbriefenc.sec";*/
+    stream = fopen(argv[1], "r");
+    if (stream == NULL) {
+        fprintf(stderr, "Fehler beim Öffnen von %s\n", argv[1]);
+    } else {
+        analyze(stream);
+        fclose(stream);
+    }
+    return 0;
 }
